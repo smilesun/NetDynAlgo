eff4a0de23631f0322adbe3a4fc02f9fefb73613
Xudong Sun <smilesun.east@gmail.com>
2022-08-26 11:22:30 +0200

    distributed sgd on absolute value fun with different centers

---
+++ dist_opt_example.py
@@ -0,0 +1,33 @@
+# Adapted from Prof A.O.
+import networkx as nx
+import numpy as np
+from matplotlib import pyplot as plt
+from fun_abs import l1grad, l1gradvec
+
+n = 5
+G = nx.star_graph(n)
+A = nx.to_numpy_array(G)
+
+def gen_interact_mat_from_adjacency(A):
+    W = (1/(2*n))*A
+    W[0][0] = 0.5
+    for node in range(1, n+1):
+        W[node][node] = 1-1/(2*n)  # fill value at diagonal element
+    return W
+
+
+W = gen_interact_mat_from_adjacency(A)
+
+# gen stochastic matrix done
+
+print(W)
+
+x = np.zeros(n+1)  # initial value of parameter
+
+vec_a = np.array([1, 2, 3.7, 3.7, 5, 6])  # each node has different function
+step = 0.001
+iters = 10000
+
+for i in range(iters):
+    x = W @ x - step*l1gradvec(x, vec_a)  # @ means matrix multiplication
+    print("parameter at each node: (consensus)", x)
